c34ecbb70040fc3f8af56f184e9b4690fae40340
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-05-31 21:50:47 +0600

    admin dashboard created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -30,6 +31,7 @@ async function run() {
     const menuCollection = client.db("sweetBitesDB").collection("menu");
     const reviewCollection = client.db("sweetBitesDB").collection("reviews");
     const cartCollection = client.db("sweetBitesDB").collection("carts");
+    const paymentCollection = client.db("sweetBitesDB").collection("payments");
 
     //jwt related api
     app.post("/jwt", async (req, res) => {
@@ -187,6 +189,66 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+    // payments intent
+    app.post("/create-payment-intent",async(req, res, ) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"]
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    })
+
+
+    app.get('/payments/:email',verifyToken, async (req, res) => {
+      const query = {email: req.params.email}
+      if(req.params.email !== req.decoded.email) {
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/payments',async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      const query = {_id: {
+        $in: payment.cartIds.map(id => new ObjectId(id))
+      }}
+      const deleteResult = await cartCollection.deleteMany(query);
+      
+      res.send({paymentResult, deleteResult});
+    })
+
+    //stats or analytics
+    app.get('/admin-stats',verifyToken,verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount()
+      const menuItems = await menuCollection.estimatedDocumentCount()
+      const orders = await paymentCollection.estimatedDocumentCount()
+      // const payments = await paymentCollection.find().toArray()
+      // expensive as loads many data not best approach
+      // const revenue = payments.reduce((total, payment) => total + payment.price,0);
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: "$price"
+            }
+          }
+        }
+      ]).toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        users,menuItems, orders, revenue
+      })
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
